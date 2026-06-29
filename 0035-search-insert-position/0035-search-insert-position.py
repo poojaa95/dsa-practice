115e91c76366467d374669fefe06758c4a24dd6c
Sree Pooja <vu.241fa04f01@gmail.com>
2026-06-29 22:57:00 +0530

    Time: 0 ms (100%), Space: 12.9 MB (29.61%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>=target:
+                return i
+        return n
+        
