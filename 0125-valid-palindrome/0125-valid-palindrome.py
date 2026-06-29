820e1b4c3d38fe3fae57211fdde69825aa16feaf
Sree Pooja <vu.241fa04f01@gmail.com>
2026-06-29 23:09:45 +0530

    Time: 10 ms (92.81%), Space: 12.5 MB (99.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s=s.lower()
+        i=0
+        j=len(s)-1
+        while i<j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i]==s[j]:
+                i+=1
+                j-=1
+            else:
+                return False
+        return True        
