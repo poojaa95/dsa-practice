792b12a64d0af2f6d7d9e210fbfb9cd77c4ffc84
Sree Pooja <vu.241fa04f01@gmail.com>
2026-07-20 21:54:36 +0530

    Time: 0 ms (100%), Space: 12.9 MB (44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        sem={}
-        for i in range(len(numbers)):
-            rem=target-numbers[i]
-            if rem in sem:
-                return (sem[rem]+1,i+1)
-            sem[numbers[i]]=i
-        
+        # sem={}
+        # for i in range(len(numbers)):
+        #     rem=target-numbers[i]
+        #     if rem in sem:
+        #         return (sem[rem]+1,i+1)
+        #     sem[numbers[i]]=i
+        left=0
+        right=len(numbers)-1
+        while left<right:
+            sem=numbers[left]+numbers[right]
+            if sem==target:
+                return (left+1,right+1)
+            elif sem>target:
+                right-=1
+            else:
+                left+=1
         
