2220b03123680f67f974235f381dff0c4e2cf3cc
Sree Pooja <vu.241fa04f01@gmail.com>
2026-07-24 11:25:29 +0530

    Time: 11 ms (96.11%), Space: 12.4 MB (98.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -22,14 +22,14 @@ class Solution(object):
         fre={}
         if len(s) != len(t):
             return False
-        for i in range(len(s)):
-            if s[i] in fre:
-                fre[s[i]] += 1
+        for i in s:
+            if i in fre:
+                fre[i] += 1
             else:
-                fre[s[i]] = 1
-        for i in range(len(t)):
-            if t[i] in fre:
-                fre[t[i]] -= 1
+                fre[i] = 1
+        for i in t:
+            if i in fre:
+                fre[i] -= 1
             else:
                 return False
         for i in fre.values():
