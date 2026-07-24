77019b331411dc85918c424edfe406b029d445e0
Sree Pooja <vu.241fa04f01@gmail.com>
2026-07-24 11:24:41 +0530

    Time: 19 ms (65.12%), Space: 14.3 MB (12.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,40 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        # fre={}
+        # que={}
+        # if len(s) != len(t):
+        #    return False
+        # for i in range(len(s)):
+        #     if s[i] in fre:
+        #         fre[s[i]] += 1
+        #     else:
+        #         fre[s[i]] = 1
+        # for j in range(len(t)):
+        #     if t[j] in que:
+        #         que[t[j]] += 1
+        #     else:
+        #         que[t[j]] = 1
+        # if fre==que:
+        #     return True
+        # else:
+        #     return False
+
         fre={}
-        que={}
         if len(s) != len(t):
-           return False
+            return False
         for i in range(len(s)):
             if s[i] in fre:
                 fre[s[i]] += 1
             else:
                 fre[s[i]] = 1
-        for j in range(len(t)):
-            if t[j] in que:
-                que[t[j]] += 1
+        for i in range(len(t)):
+            if t[i] in fre:
+                fre[t[i]] -= 1
             else:
-                que[t[j]] = 1
-        if fre==que:
-            return True
-        else:
-            return False
+                return False
+        for i in fre.values():
+            if i!=0:
+                return False
+        return True
             
         
