8352fdf8af05996ede04fa547f70ec7e1336880d
Sree Pooja <vu.241fa04f01@gmail.com>
2026-07-31 22:47:36 +0530

    Time: 0 ms (100%), Space: 12.7 MB (38.93%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,7 +1,19 @@
 class Solution(object):
     def search(self, nums, target):
-        for i in nums:
-            if target==i:
-                return True
+        # for i in nums:
+        #     if target==i:
+        #         return True
+        # return False
+        low=0
+        high=len(nums)-1
+        if low<=high:
+            mid=(low+high)//2
+            for i in nums:
+                if i==target:
+                    return True
+                elif i>target:
+                    low=mid+1
+                else:
+                    high=mid-1
         return False
         
