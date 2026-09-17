a6376c7b28e5f08300159239129ccbcbf373f5ad
Sree Pooja <vu.241fa04f01@gmail.com>
2026-09-17 15:36:32 +0530

    Time: 7 ms (56.37%), Space: 13.6 MB (20.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        k=[]
-        for i in nums[:]:
-            if i==0:
-                k.append(i)
-                nums.remove(i)
-        nums=nums.extend(k)
+        # k=[]
+        # for i in nums[:]:
+        #     if i==0:
+        #         k.append(i)
+        #         nums.remove(i)
+        # nums=nums.extend(k)
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[k]=nums[i]
+                k+=1
+        for i in range(k,len(nums)):
+            nums[i]=0
+
             
         
