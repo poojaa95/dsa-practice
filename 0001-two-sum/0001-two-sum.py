a41359cbd7d23e3456007a4e7645212549b35874
Sree Pooja <vu.241fa04f01@gmail.com>
2026-09-18 16:51:18 +0530

    Time: 12 ms (44.89%), Space: 13.9 MB (17.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,11 +5,21 @@ class Solution(object):
         #     for j in range(i+1,len(nums)):
         #         if nums[i]+nums[j]==target:
         #             return i,j
-        sem={}
-        for i in range(len(nums)):
-            rem=target-nums[i]
-            if rem in sem:
-                return [sem[rem],i]
-            sem[nums[i]]=i
+        arr = []
 
-        
+        for i in range(len(nums)):
+            arr.append([nums[i], i])
+
+        arr.sort()
+
+        left, right = 0, len(arr)-1
+
+        while left < right:
+            if arr[left][0] + arr[right][0] == target:
+                return [arr[left][1], arr[right][1]]
+            elif arr[left][0] + arr[right][0] > target:
+                right -= 1
+            else:
+                left += 1
+
+        return []
